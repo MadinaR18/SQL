0119439f2fbec5d9cb2e036ca43ae7f4db08bade
MadinaR18 <mdnerashidova98@gmail.com>
2025-07-06 22:17:04 +0700

    Add files via upload

---
+++ lesson-3/homework/SQLQuery1homework3.sql
@@ -0,0 +1,44 @@
+--create database homework 
+ --create table employees_information (id int, name varchar (50))
+ 
+ --select * from employees_information;
+ --insert into employees_information values (1,'John'), (2,'Sam'), (3, 'Eddie') 
+ --insert into employees_information values (4,'Omar'), (5,'Mike'), (6,'Selene') 
+ --insert into employees_information values (7,'Anna'), (8,'Ali'), (9, 'Rose')  
+ --insert into employees_information values (10,'Rick'), (11,'Taylor')
+
+
+--create table departments (id int, name varchar (60)) 
+
+--select * from departments;
+
+--insert into departments values (1,'human resources'), (2, 'marketing'), (3,'Pr') 
+--insert into departments values (4,'Finance'), (5,'IT Support'), (6, 'Accounting') 
+--insert into departments values (7,'Tax'), (8,'Risk management'), (9,'Management') 
+--insert into departments values (10,'Insurance') 
+
+
+--create table projects (id int, name varchar (60), email varchar (60))
+
+--select * from projects;
+--insert into projects values (1,'Redesign', 'designt@com'), (2,'Survey','customer@com'), (3,'Healthy food','diet@com') 
+--insert into projects values (4,'Website design', 'webdesign@com'), (5,'Investment','invest@com'),(6,'Funding','funding@com')
+--insert into projects values (7,'Capitalism', 'capit@com'), (8,'Budgeting', 'budget@com'), (9,'cryptolife', 'crypto@com'), (10,'Fintechnology', 'fintech@com') 
+
+
+--create table positions (id int, name varchar (50),position varchar (60))
+
+--select * from positions;
+--insert into positions values (1,'John', 'Software Engineer'), (2,'Sam','Business Analyst'), (3,'Eddie', 'IT Technician') 
+--insert into positions values ( 4,'Omar', 'Accountant'), (5,'Mike','Legal advisor'), (6,'Selene', 'HR Manager'), (7,'Anna', 'Customer Support')
+--insert into positions values (8,'Ali', 'Data Analyst'), (9,'Rose','Marketing Specialist'), (10,'Rick', 'PR') 
+ 
+ 
+
+
+ --create table people (id int, name varchar (50),age int)
+
+ --select * from people;
+
+ --insert into people values (1,'John','33'), (2,'Sam','29'), (3,'Eddie','25'),(4,'Omar','36'),(5,'Mike','30'),(6,'selene','35')
+ --insert into people values (7,'Anna','30'), (8,'Ali','24'), (9,'Rose','37'), (10,'Rick','28') 
